72f2678e40c0c03bdfe72128a02db0a981de47e1
Jonatas Serra <jonatasserra@outlook.com>
2023-11-19 14:32:52 -0300

    Alteracao de API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-my-finance-production.up.railway.app",
+  baseURL: "https://api-my-finance.onrender.com",
 });
 
 export default api;
