c23fdb2342b1aa2716f1e2808172faa4f184a5a9
Jonatas Serra <jonatasserra@outlook.com>
2024-06-14 00:20:39 -0300

    ajuste deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://api-myfinance-326ee4ab2f67.herokuapp.com',
 })
 
 export default api
