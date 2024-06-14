6020b0ae5916037f8da731d4d506ed803e238b8e
Jonatas Serra <jonatasserra@outlook.com>
2024-06-14 00:17:35 -0300

    ajuste

--- src/Pages/Settings/index.tsx
+++ src/Pages/Settings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useUser } from '../../hooks/useUser'
 import { useToast } from '../../hooks/useToast'
 import { useAccounts } from '../../hooks/useAccounts'
