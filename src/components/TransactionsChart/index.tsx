49e68754ffb60d6e208cec66f0796d7159f13784
Jonatas Serra <jonatasserra@outlook.com>
2024-06-18 11:21:12 -0300

    fix(frontend): correct date formatting bug in summary screen chart

--- src/components/TransactionsChart/index.tsx
+++ src/components/TransactionsChart/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
 import { useTransactions } from '../../hooks/useTransactions'
-
 import { Spinner, Container } from './styles'
+import { format, parseISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
-// Hook para obter a largura da janela em tempo real
 const useWindowWidth = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
@@ -17,8 +17,23 @@ const useWindowWidth = () => {
   return windowWidth
 }
 
+interface Transaction {
+  _id: string
+  description: string
+  type: string
+  category: string
+  amount: number
+  createdAt: string
+  createdBy: string
+  walletId: string
+  date: string
+}
+
 const TransactionsChart: React.FC = () => {
   const { transactions } = useTransactions()
+  const [formattedTransactions, setFormattedTransactions] = useState<
+    Transaction[]
+  >([])
   const [chartData, setChartData] = useState<any>(null)
   const windowWidth = useWindowWidth()
 
@@ -35,19 +50,29 @@ const TransactionsChart: React.FC = () => {
   }
 
   useEffect(() => {
+    const formatted = transactions.map((transaction) => ({
+      ...transaction,
+      date: transaction.date.split('T')[0], // Removendo a parte de tempo da data
+    }))
+    setFormattedTransactions(formatted)
+    console.log('transactions', transactions)
+    console.log('Formatada', formatted)
+  }, [transactions])
+
+  useEffect(() => {
     const last6Months = Array.from({ length: 6 }, (_, i) => {
       const date = new Date()
       date.setMonth(date.getMonth() - i)
-      return date.toLocaleString('default', { month: 'short' })
+      return format(date, 'MMM yyyy', { locale: ptBR })
     }).reverse()
 
     const deposits = new Array(6).fill(0)
     const withdrawals = new Array(6).fill(0)
 
-    transactions.forEach((transaction) => {
-      const date = new Date(transaction.date)
-      const month = date.toLocaleString('default', { month: 'short' })
-      const index = last6Months.indexOf(month)
+    formattedTransactions.forEach((transaction) => {
+      const date = parseISO(transaction.date)
+      const formattedMonth = format(date, 'MMM yyyy', { locale: ptBR })
+      const index = last6Months.indexOf(formattedMonth)
 
       if (index !== -1) {
         if (transaction.type === 'Deposit') {
@@ -71,12 +96,12 @@ const TransactionsChart: React.FC = () => {
           color: '#dc2020',
         },
       ],
-      resposive: [
+      responsive: [
         {
           breakpoint: 768,
           options: {
             chart: {
-              with: '100%',
+              width: '100%',
             },
             plotOptions: {
               bar: {
@@ -157,7 +182,7 @@ const TransactionsChart: React.FC = () => {
         },
       },
     })
-  }, [transactions])
+  }, [formattedTransactions])
 
   if (!chartData) {
     return (
