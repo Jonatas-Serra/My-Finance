809bcd0b6f1a91bc59c059b5001096188c67979c
Jonatas Serra <jonatasserra@outlook.com>
2025-02-23 23:24:53 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api-myfinance-326ee4ab2f67.herokuapp.com',
+  baseURL: 'https://api-myfinance-66b73cbfa2ad.herokuapp.com',
 })
 
 export default api
